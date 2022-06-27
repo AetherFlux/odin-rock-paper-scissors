f3c0cdc86900a593370fe16e5466fea8a72caa49
Lars Thomsen <TheThomsen05@gmail.com>
2022-06-27 17:02:19 -0400

    Setup 5 round game logic

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+let score = 0;
+
 function computerPlay () {
     choise = Math.floor(Math.random()*3)
     if (choise === 0) {
@@ -18,14 +20,30 @@ function computerPlay () {
          (playerSelection == "paper" && computerSelection == "rock") ||
          (playerSelection == "scissors" && computerSelection == "paper")
      ) {
-         console.log(playerSelection + " beats " + computerSelection + ", you win!")
+         console.log(playerSelection + " beats " + computerSelection + ", you win!");
+         score ++;
      } else if (
         (computerSelection == "rock" && playerSelection == "scissors") ||
         (computerSelection == "paper" && playerSelection == "rock") ||
         (computerSelection == "scissors" && playerSelection == "paper")
      ) {
-         console.log(computerSelection + " beats " + playerSelection + ", you lose!")
+         console.log(computerSelection + " beats " + playerSelection + ", you lose!");
+         score --;
      }
  }
 
- playRound();
+function game () {
+    for (i=0; i<5; i++) {
+        playRound();
+    }
+
+    if (score > 0) {
+        console.log("Player wins!")
+    } else if (score < 0) {
+        console.log("Computer wins!")
+    } else {
+        console.log("The game is a tie!")
+    }
+}
+
+game();
