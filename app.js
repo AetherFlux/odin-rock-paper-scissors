baa840983a7e0f83a66abf210a3376f06d194469
Lars Thomsen <TheThomsen05@gmail.com>
2022-06-27 16:40:11 -0400

    Setup round logic

--- app.js
+++ app.js
@@ -8,3 +8,24 @@ function computerPlay () {
         return "SCISSORS"
     }
 }
+ function playRound (playerSelection, computerSelection) {
+     computerSelection = computerPlay().toLowerCase();
+     playerSelection = prompt("Select your play").toLowerCase();
+     if (playerSelection === computerSelection) {
+         console.log("it's a tie!");
+     } else if (
+         (playerSelection == "rock" && computerSelection == "scissors") ||
+         (playerSelection == "paper" && computerSelection == "rock") ||
+         (playerSelection == "scissors" && computerSelection == "paper")
+     ) {
+         console.log(playerSelection + " beats " + computerSelection + ", you win!")
+     } else if (
+        (computerSelection == "rock" && playerSelection == "scissors") ||
+        (computerSelection == "paper" && playerSelection == "rock") ||
+        (computerSelection == "scissors" && playerSelection == "paper")
+     ) {
+         console.log(computerSelection + " beats " + playerSelection + ", you lose!")
+     }
+ }
+
+ playRound();
