98c8c1d33151e15830ec4db633921b89713af0ce
Lars Thomsen <TheThomsen05@gmail.com>
2022-07-08 04:17:29 -0400

    Hook up game logic to new UI

--- app.js
+++ app.js
@@ -1,59 +1,99 @@
-let score = 0;
+let playerScore = 0;
+let computerScore = 0;
 const buttons = document.querySelectorAll('button');
+const displayWin = document.querySelector('.result-win');
+const displayLoss = document.querySelector('.result-loss');
+const displayTie = document.querySelector('.result-tie');
+
+let playerWins = [];
+let computerWins = [];
+
+document.querySelectorAll('.player-wins > .count').forEach(score => playerWins.push(score));
+document.querySelectorAll('.computer-wins > .count').forEach(score => computerWins.push(score));
 
 buttons.forEach(button => button.addEventListener('click', () => {
     console.log(playRound(button.id))
 }));
 
+function displayRandom () {
+    document.querySelector('#computer-paper').classList.toggle('neon-green-box');
+    document.querySelector('#computer-rock > i').classList.toggle('neon-green');
+    setTimeout(() => {
+        document.querySelector('#computer-rock').classList.toggle('neon-green-box');
+        document.querySelector('#computer-rock > i').classList.toggle('neon-green');
+    }, 800);
+}
 
 function computerPlay () {
-    choise = Math.floor(Math.random()*3)
+    choise = Math.floor(Math.random()*3);
     if (choise === 0) {
+        document.querySelector('#computer-rock').classList.toggle('neon-green-box');
+        document.querySelector('#computer-rock > i').classList.toggle('neon-green');
+        setTimeout(() => {
+            document.querySelector('#computer-rock').classList.toggle('neon-green-box');
+            document.querySelector('#computer-rock > i').classList.toggle('neon-green');
+        }, 1500);
         return "ROCK"
     } else if (choise === 1) {
+        document.querySelector('#computer-paper').classList.toggle('neon-blue-box');
+        document.querySelector('#computer-paper > i').classList.toggle('neon-blue');
+        setTimeout(() => {
+            document.querySelector('#computer-paper').classList.toggle('neon-blue-box');
+            document.querySelector('#computer-paper > i').classList.toggle('neon-blue');
+        }, 1500);
         return "PAPER"
     } else if (choise === 2) {
+        document.querySelector('#computer-scissors').classList.toggle('neon-red-box');
+        document.querySelector('#computer-scissors > i').classList.toggle('neon-red');
+        setTimeout(() => {
+            document.querySelector('#computer-scissors').classList.toggle('neon-red-box');
+            document.querySelector('#computer-scissors> i').classList.toggle('neon-red');
+        }, 1500);
         return "SCISSORS"
     }
 }
  function playRound (playerSelection) {
      const computerSelection = computerPlay().toLowerCase();
      playerSelection = playerSelection.toLowerCase();
+     if (playerScore === 5 || computerScore === 5) {
+         playerScore = 0;
+         computerScore = 0;
+         document.querySelector('#player-trophy').classList.remove('neon-blue');
+         document.querySelector('#computer-trophy').classList.remove('neon-red');
+         for (i = 1; i < 6; i++){
+            playerWins[i].classList.remove('neon-blue');
+            computerWins[i].classList.remove('neon-red');
+         }
+     }
      if (playerSelection === computerSelection) {
+        displayTie.classList.toggle('neon-green');
+        setTimeout(() => displayTie.classList.toggle('neon-green'), 1500);
          return "it's a tie!";
      } else if (
          (playerSelection == "rock" && computerSelection == "scissors") ||
          (playerSelection == "paper" && computerSelection == "rock") ||
          (playerSelection == "scissors" && computerSelection == "paper")
      ) {
-         score ++;
-         return playerSelection + " beats " + computerSelection + ", you win!";
+         playerScore ++;
+         playerWins[playerScore].classList.toggle('neon-blue');
+         if (playerScore === 5) {
+            document.querySelector('#player-trophy').classList.add('neon-blue');
+        }
+         displayWin.classList.toggle('neon-blue');
+         setTimeout(() => displayWin.classList.toggle('neon-blue'), 1500);
+         return
      } else if (
         (computerSelection == "rock" && playerSelection == "scissors") ||
         (computerSelection == "paper" && playerSelection == "rock") ||
         (computerSelection == "scissors" && playerSelection == "paper")
      ) {
-         score --;
-         return computerSelection + " beats " + playerSelection + ", you lose!";
+         computerScore ++;
+         computerWins[computerScore].classList.toggle('neon-red');
+         if (computerScore === 5) {
+             document.querySelector('#computer-trophy').classList.add('neon-red');
+         }
+         displayLoss.classList.toggle('neon-red');
+         setTimeout(() => displayLoss.classList.toggle('neon-red'), 1500);
+         return
      }
  }
-
-
- /*
-function game () {
-    for (i=0; i<5; i++) {
-        console.log(playRound());
-    }
-
-    if (score > 0) {
-        console.log("Player wins!")
-    } else if (score < 0) {
-        console.log("Computer wins!")
-    } else {
-        console.log("The game is a tie!")
-    }
-}
-
-game();
-*/
-
